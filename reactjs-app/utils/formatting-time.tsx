ee4f44790c32d1b39e1b9971806c7d351c38d4e6
Dmitry Arhitector <dmitry.arhitector@yandex.ru>
2022-08-03 14:44:02 +0300

    fix: ошибка в форматировании часов\минут через :

--- reactjs-app/utils/formatting-time.tsx
+++ reactjs-app/utils/formatting-time.tsx
@@ -7,5 +7,5 @@
 export const formattingTime = (value: number) => {
     const onlyTimeString = new Date(value * 1000).toISOString().substring(11, 11 + 8);
 
-    return onlyTimeString.indexOf('00:') === 0 ? onlyTimeString.substring(3) : value;
+    return onlyTimeString.indexOf('00:') === 0 ? onlyTimeString.substring(3) : onlyTimeString;
 }
